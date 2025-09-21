481df79b2571ea06bfb76e21abb26d0693dee593
GSkorpik <gosha.midural@gmail.com>
2025-09-21 12:37:11 +0500

    19-21

--- 19-21/48.py
+++ 19-21/48.py
@@ -1,15 +1,16 @@
 def f(s, m):
-    if s >= 43: return m % 2 == 0
+    if 43<=s<=72: return m % 2 == 0
+    if s>72: return m%2!=0
     if m == 0: return 0
     if m % 2 != 0:
-        return f(s *3 , m - 1) or f(s * 2, m - 1)
+        return f(s * 3 , m - 1) or f(s * 2, m - 1) or  f(s+1,m-1)
     else:
-        return f(s * 1, m - 1) and f(s * 2, m - 1)
+        return f(s * 3, m - 1) and f(s * 2, m - 1) and f(s+1,m-1)
 
 
-print('19)', *[s for s in range(1, 43) if f(s, )])
-print('20)', *[s for s in range(1, 43) if not f(s, )])
-print('21)', *[s for s in range(1, 43) if not f(s, )])
+print('19)', *[s for s in range(1, 43) if f(s,2 )])
+print('20)', *[s for s in range(1, 43) if not f(s, 1) and f(s,3)])
+print('21)', *[s for s in range(1, 43) if not f(s, 2) and f(s,4)])
 
 
 
