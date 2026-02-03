d096e59718559e6e643b0acfb82291378222605d
GSkorpik <gosha.midural@gmail.com>
2026-02-03 18:12:01 +0500

    13

--- 123.py
+++ 123.py
@@ -1,8 +1,2 @@
-print('x y z w | F')
-for x in 0,1:
-    for y in 0,1:
-        for z in 0,1:
-            for w in 0,1:
-                f=(not(w)and z and not(y) and not(x)) or (not(w) and z and y and not(x)) or (not(w) and z and y and x)
-                if f ==1:
-                    print(x,y,z,w,f)
+print(bin(240)[2:])
+print(int('11111100',2))
